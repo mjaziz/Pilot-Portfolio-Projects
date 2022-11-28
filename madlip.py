dbb56daab54a7d63b513e6c959649d605cf886c2
mjaziz <115729108+mjaziz@users.noreply.github.com>
2022-11-28 12:35:31 -0500

    Update and rename README.md to madlip.py

---
+++ madlip.py
@@ -0,0 +1,10 @@
+# Pilot-Portfolio-Projects
+
+adj = input("adjective: ")
+verb1 = input("verb: ")
+verb2 = input("verb: ")
+Successful_person = input("Successful person: ") 
+
+madlib = f"Coding is so {adj}! It is so exciting because \ I lover {verb1}. Stay curious and {verb2} like you are {Successful_person}!"
+
+print(madlib)
